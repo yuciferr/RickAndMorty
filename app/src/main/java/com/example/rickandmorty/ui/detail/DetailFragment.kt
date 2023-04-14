acd68ac3f9291153771138dabe1475ea3ba6e13b
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-04-14 20:16:24 +0300

    detail fragment set up

--- app/src/main/java/com/example/rickandmorty/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/rickandmorty/ui/detail/DetailFragment.kt
@@ -1,32 +1,61 @@
 package com.example.rickandmorty.ui.detail
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.rickandmorty.R
+import com.example.rickandmorty.databinding.FragmentDetailBinding
+
 
 class DetailFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = DetailFragment()
-    }
-
-    private lateinit var viewModel: DetailViewModel
+    private lateinit var binding: FragmentDetailBinding
+    private val navArgs by navArgs<DetailFragmentArgs>()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    ): View {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+
+        binding.apply {
+            nameTv.text = navArgs.character.name
+            statusTv.text = navArgs.character.status
+            specyTv.text = navArgs.character.species
+            genderTv.text = navArgs.character.gender
+            locationTv.text = navArgs.character.location
+            originTv.text = navArgs.character.origin
+            episodesTv.text = getEpisodes(navArgs.character.episode)
+            createdAtTv.text = navArgs.character.created
+            Glide.with(this@DetailFragment).load(navArgs.character.image)
+                .placeholder(android.R.drawable.ic_menu_report_image).into(imageIv)
+
+
+            backBtn.setOnClickListener {
+                it.findNavController().navigate(R.id.action_detailFragment_to_homeFragment)
+            }
+
+        }
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
+    private fun getEpisodes(episodes: List<String?>?) : String{
+        var episodesString = episodes?.first()?.split("/")?.last()
+
+        for (episode in episodes?.drop(1)!!){
+            if (episode != null) {
+                episodesString += " ,${episode.split("/").last()}"
+            }
+        }
+
+        return episodesString.toString()
+    }
 }
