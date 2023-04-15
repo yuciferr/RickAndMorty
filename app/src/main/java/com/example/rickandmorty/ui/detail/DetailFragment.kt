37990d8e6c98ca5fd1a0ff72a9f523f5413ccfe8
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-04-16 00:39:42 +0300

    Bug Fixed: Created at Date format

--- app/src/main/java/com/example/rickandmorty/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/rickandmorty/ui/detail/DetailFragment.kt
@@ -1,6 +1,7 @@
 package com.example.rickandmorty.ui.detail
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -31,7 +32,8 @@ class DetailFragment : Fragment() {
             locationTv.text = navArgs.character.location
             originTv.text = navArgs.character.origin
             episodesTv.text = getEpisodes(navArgs.character.episode)
-            createdAtTv.text = navArgs.character.created
+            createdAtTv.text = dateToTime(navArgs.character.created.toString())
+            Log.d("YUSUF", "${navArgs.character.created?.let { dateToTime(it) }})}")
             Glide.with(this@DetailFragment).load(navArgs.character.image)
                 .placeholder(android.R.drawable.ic_menu_report_image).into(imageIv)
 
@@ -56,5 +58,29 @@ class DetailFragment : Fragment() {
         }
 
         return episodesString.toString()
+    }
+
+    private fun dateToTime(date: String): String {
+        val array = date.split("T")
+        val dateArray = array[0].split("-").toMutableList()
+        dateArray[1] = when (dateArray[1]) {
+            "01" -> "January"
+            "02" -> "February"
+            "03" -> "March"
+            "04" -> "April"
+            "05" -> "May"
+            "06" -> "June"
+            "07" -> "July"
+            "08" -> "August"
+            "09" -> "September"
+            "10" -> "October"
+            "11" -> "November"
+            "12" -> "December"
+            else -> "January"
+        }
+        val newDate = "${dateArray[2]} ${dateArray[1]} ${dateArray[0]}"
+
+        val timeArray = array[1].split(":")
+        return "$newDate, ${timeArray[0]}:${timeArray[1]}"
     }
 }
